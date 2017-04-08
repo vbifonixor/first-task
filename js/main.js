7981505cc7ba663700f66fcd826cedf300185f14
Mikhail Kononenko <i@vbifonix.ru>
2017-04-09 01:30:12 +0300

    Edge and Safari related issues fixed

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let teachers = document.querySelectorAll('.schedule-card-lecture-teacher > a');
 
 // Вешаем на каждое имя учителя показ модалки по клику
 
-teachers.forEach((teacher, index, arr) => {
+Array.prototype.forEach.call(teachers, (teacher, index, arr) => {
   let modalHash = teacher.href.split('#')[1];
   teacher.addEventListener('click', (e) => {
     e.preventDefault();
@@ -10,17 +10,17 @@ teachers.forEach((teacher, index, arr) => {
     modal.setAttribute('data-shown', true);
     BNS.on(); // и убираем скролл у body
   })
-});
+}); // Edge hack
 
 
 let closes = document.querySelectorAll('.lecturer-modal-close');
 
 // На каждую кнопку закрытия модалки вешаем закрытие по клику
 
-closes.forEach((close, index, arr) => {
+Array.prototype.forEach.call(closes, (close, index, arr) => {
   close.addEventListener('click', (e) => {
     let modal = close.parentElement.parentElement; // выбираем элемент модалки
     modal.setAttribute('data-shown', false); // закрываем модалку
     BNS.off(); // возвращаем body его законный скролл, когда модалка закрыта
   })
-})
+}) // Edge hack. Опять
